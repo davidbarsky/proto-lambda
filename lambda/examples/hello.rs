47e3593333963bf0b4964887179d38b80d048d77
David Barsky <dbarsky@amazon.com>
2019-06-17 17:14:33 -0400

    call out lack of context

--- lambda/examples/hello.rs
+++ lambda/examples/hello.rs
@@ -6,5 +6,7 @@ type Err = Box<dyn std::error::Error + Send + Sync + 'static>;
 #[lambda]
 #[runtime::main]
 async fn main(event: String) -> Result<String, Err> {
+    // notice the absence of the context. It'll be provided through
+    // an as-of-yet implemented task-local.
     Ok(event)
 }
